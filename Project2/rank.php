a8a92bdf65ded6d505d8a97a5c1c872431cdec75
Harsh Jivani <harsh.jivani7@gmail.com>
2020-03-17 17:06:57 -0400

    Add files via upload

--- Project2/rank.php
+++ Project2/rank.php
@@ -1,3 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Ranking Page</title>
+    <link rel="stylesheet" href="styles.css">
+</head>
+
+<body>
+
+<div class="outters">
 <?php
     session_start();
     $_SESSION['ranking'][$_SESSION['Username']] = $_SESSION['numbWin'];
@@ -16,24 +28,18 @@
     }
 
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Ranking Page</title>
-    <link rel="stylesheet" href="styles.css">
-</head>
-
-<body>
-
-<div class="wrap">
+</div>
+<div class="result">
+<br><br><br>
+    <br><br><br>
+    <div>
         <form class="login-form" action="login.php" method="post">
-        <div class="form-group">
+            <div class="form-group">
                 <button class="form-button" name="Submit" value="Logout" type="submit">Logout</button>
             </div>
         </form>
     </div>
+</div>
 
 </body>
 </html>
